64b9d54c3e6666c8f5d421624cd5e5b916663f75
Hlina21 <hlina.nova@gmail.com>
2023-03-09 10:17:45 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """print the number of and list of arguments"""
-    import sys
+import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+if __name__ == "__main__":
+    args = sys.argv
+    argc = len(args)
+    msg = ""
+    if argc == 1:
+        msg = "arguments."
+    elif argc == 2:
+        msg = "argument:"
     else:
-        print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}:{}".format(i + 1, sys.argv[i + 1]))
+        msg = "arguments:"
+
+        print("{:d} {}".format(argc - 1, msg))
+        for i in range(1, argc):
+            print("{:d}: {}".format(i, args[i]))
